a120eeeea04318554ee70ae082660303b0038b60
Iryoda <iryodap@gmail.com>
2021-07-04 17:13:18 -0300

    fix: build errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,8 @@
 import { NextPage } from 'next';
-import * as S from './styles';
+import HomeTemplate from 'template/Home';
 
 const Home: NextPage = () => {
-    return (
-        <S.Container>
-            <S.Title>
-                Esse template foi criado pela equipe de Research 21.1 para
-                projetos internos da Polijunior.
-            </S.Title>
-        </S.Container>
-    );
+    return <HomeTemplate />;
 };
 
 export default Home;
