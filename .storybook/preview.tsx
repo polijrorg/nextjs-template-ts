c1f562a042a740056c12c3ff47c47360df2a4c06
Augusto Iryoda <iryodap@gmail.com>
2021-11-11 22:33:37 -0300

    :sparkles: feat: Mui stoybook config

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable*/
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from '../src/styles';
+import { muiTheme } from 'storybook-addon-material-ui';
 import { theme } from '../src/styles/default.theme';
 import * as nextImage from 'next/image';
 
@@ -14,5 +16,6 @@ export const decorators = [
             <GlobalStyles />
             <Story />
         </ThemeProvider>
-    )
+    ),
+    muiTheme()
 ];
