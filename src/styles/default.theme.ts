a93d46b88d5ccc3555e3fd7c37412366e9419ed9
IagoErrera <iagoerrera@hotmail.com>
2023-02-25 15:51:51 -0300

    add: design system base files

--- src/styles/default.theme.ts
+++ src/styles/default.theme.ts
@@ -1,23 +1,55 @@
-export const theme = {
+export const defaultTheme = {
     colors: {
         primary: {
-            main: '#004C97',
-            light: '#378CDF',
-            dark: '#002549'
+            light: 'hsl(205 ,74%, 65%)',
+            main: 'hsl(205, 67%, 45%)',
+            dark: 'hsl(205, 87%, 29%)'
         },
-
-        secundary: {
-            main: '#FCA925',
-            light: '#FFD797',
-            dark: '#CD7E00'
-        },
-
-        highEmphasis: '#1E2732',
-        mediumEmphasis: '#545454',
-        lowEmphasis: '#949494',
-
-        surface: '#FFFFFF',
-        divider: '#E6EAED',
-        background: '#FAFAFA'
-    }
+        secondary: {
+            light: 'hsl(174, 96%, 78%)',
+            main: 'hsl(180, 77%, 47%)',
+            dark: 'hsl(186, 91%, 29%)'
+        }
+    },
+    space: [4, 8, 12, 16, 24, 32, 48, 64, 96, 128, 192, 256],
+    radii: {
+        sm: 8,
+        md: 16,
+        lg: 32
+    },
+    fontSizes: {
+        1: '12px',
+        2: '13px',
+        3: '15px'
+    },
+    fonts: {
+        untitled: 'Untitled Sans, apple-system, sans-serif',
+        mono: 'Söhne Mono, menlo, monospace'
+    },
+    fontWeights: {},
+    lineHeights: {},
+    letterSpacings: {},
+    sizes: {},
+    borderWidths: {},
+    borderStyles: {},
+    shadows: {},
+    zIndices: {},
+    transitions: {}
 };
+
+export type Theme = typeof defaultTheme;
+
+// props that later will be injected by styled-components
+export type ThemeProps = { theme?: Theme };
+
+// 'primary' | 'secondary'
+export type ColorType = keyof Theme['colors'];
+
+// 3 possible shades of a ColorType
+export type ColorIndex = 'light' | 'main' | 'dark';
+
+// 12 possible spaces
+export type SpaceIndex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11;
+
+// 3 possible border radius
+export type RadiiIndex = 'sm' | 'md' | 'lg';
--- src/utils/testUtils.tsx
+++ src/utils/testUtils.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { ThemeProvider } from 'styled-components';
-import { theme } from '../styles/default.theme';
+import { defaultTheme } from '../styles/default.theme';
 
 const customRender = (ui: JSX.Element, { ...renderOptions } = {}) =>
-    render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>, renderOptions);
+    render(
+        <ThemeProvider theme={defaultTheme}>{ui}</ThemeProvider>,
+        renderOptions
+    );
 
 export * from '@testing-library/react';
 
